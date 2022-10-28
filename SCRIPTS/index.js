8ee60c7fbe14c472a3c433d56ef50f88a70f6423
rjmn02 <rhyssjmn@gmail.com>
2022-10-28 14:53:18 +0800

    Update index.js

---
+++ SCRIPTS/index.js
@@ -0,0 +1,21 @@
+// ASSIGNMENT 1 FUNCTIONS
+
+
+function ftc(){
+    let text = document.getElementById("tempInC")
+    let fahrenheit = document.getElementById("fahrenheit").value
+    let celsius = (fahrenheit-32)*(5/9)
+    text.textContent += celsius
+}
+
+function ctf(){
+    
+}
+
+function mtf(){
+    
+}
+
+function ftm(){
+
+}
