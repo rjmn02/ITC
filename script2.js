e413ce9d8b81c514ceeb295da789e01895f5e566
rjmn02 <rhyssjmn@gmail.com>
2022-10-28 19:46:20 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,22 +7,22 @@ function compute(){
         incomeTax = 0
         result.textContent = "INCOME TAX: PHP " + incomeTax 
 
-    }else if(income > 250000 && income < 400000){
+    }else if(income > 250000 && income <= 400000){
 
         incomeTax = (income-250000)*0.20
         result.textContent = "INCOME TAX: PHP " + incomeTax 
 
-    }else if(income > 400000 && income < 800000){
+    }else if(income > 400000 && income <= 800000){
 
         incomeTax = 30000+((income-400000)*0.25)
         result.textContent = "INCOME TAX: PHP " + incomeTax 
 
-    }else if(income > 800000 && income < 2000000){
+    }else if(income > 800000 && income <= 2000000){
 
         incomeTax = 130000+((income-800000)*0.30)
         result.textContent = "INCOME TAX: PHP " + incomeTax 
 
-    }else if(income > 2000000 && income < 8000000){
+    }else if(income > 2000000 && income <= 8000000){
  
         incomeTax = 490000+((income-2000000)*0.32)
         result.textContent = "INCOME TAX: PHP " + incomeTax 
