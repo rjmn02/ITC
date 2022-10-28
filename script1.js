26993d388357461987650f733a4f5337a79d3074
rjmn02 <rhyssjmn@gmail.com>
2022-10-28 15:57:23 +0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,29 @@
+// ASSIGNMENT 1 FUNCTIONS
+
+function ftc(){
+    let text = document.getElementById("tempInC")
+    let fahrenheit = document.getElementById("fahrenheit").value
+    let celsius = (fahrenheit-32)*(5/9)
+    text.textContent = celsius + " C"
+}
+
+function ctf(){
+    let text = document.getElementById("tempInF")
+    let celsius = document.getElementById("celsius").value
+    let fahrenheit = (celsius*9/5)+32
+    text.textContent = fahrenheit + " F"
+}
+
+function mtf(){
+    let text = document.getElementById("measurementInFt")
+    let meters = document.getElementById("meters").value
+    let feet = meters*3.281
+    text.textContent = feet + " ft"
+}
+
+function ftm(){
+    let text = document.getElementById("measurementInMeters")
+    let feet = document.getElementById("feet").value
+    let meters = feet/3.281
+    text.textContent = meters + " m"
+}
